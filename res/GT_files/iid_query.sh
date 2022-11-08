a3c737ebd84090cda4825591f3543587d6754034
jimmymcheung <64972743+jimmymcheung@users.noreply.github.com>
2022-11-08 12:31:57 +0100

    updated iid query script

--- res/GT_files/iid_query.sh
+++ res/GT_files/iid_query.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
+if [ ! -d "./iid" ]
+then
+        mkdir ./iid
+fi
+
+# export iid
 for n in *.gt
 do
         echo "Reading $n"
-        cat "$n" | grep '^i[0-9]+*' > "${n%.gt}.txt"
-        echo "Finish with $n"
+        m="./iid/${n%.gt}_iid.gt"
+        cat "$n" | grep '^i[0-9]+*' > "$m"
+        echo "Finish with $n, written in $m"
 done
