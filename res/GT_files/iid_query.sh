637737690422b37654dcaa86899fcc5cf785c70a
jimmymcheung <64972743+jimmymcheung@users.noreply.github.com>
2022-11-08 12:43:41 +0100

    fixed grammar

--- res/GT_files/iid_query.sh
+++ res/GT_files/iid_query.sh
@@ -11,5 +11,5 @@ do
         echo "Reading $n"
         m="./iid/${n%.gt}_iid.gt"
         cat "$n" | grep '^i[0-9]+*' > "$m"
-        echo "Finish with $n, written in $m"
+        echo "Finished with $n, written in $m"
 done
