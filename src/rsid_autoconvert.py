a779a5a67f05c0e262c1dc01c4ed9941a80bd7e4
jimmymcheung <64972743+jimmymcheung@users.noreply.github.com>
2022-11-09 11:32:56 +0100

    added file for rsid autoconvertion

---
+++ src/rsid_autoconvert.py
@@ -0,0 +1,6 @@
+#! /bin/python3
+# Private ID auto-convert to rsid
+# 2022 © Check De Test Project
+# This script convert the private ID SNP records with RSID queried from ClinVar.
+# The input should be tab-separated value, in the following format:
+# rsid	chromosome	position	genotype
