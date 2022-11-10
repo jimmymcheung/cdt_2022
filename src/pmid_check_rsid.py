af463ccba92d7754ea379775745078f5a0e66a82
jimmymcheung <64972743+jimmymcheung@users.noreply.github.com>
2022-11-10 14:27:01 +0100

    pmid request from ClinVar

---
+++ src/pmid_check_rsid.py
@@ -0,0 +1,10 @@
+# Library
+import requests
+
+# file input
+# search qeury per rsid entry from file
+for n in ...:
+    rsidv=$n
+    # URL not larger than 2048 char
+    r = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=snp&id=" +str(rsidv)+ "retmode=xml")
+print(r)
