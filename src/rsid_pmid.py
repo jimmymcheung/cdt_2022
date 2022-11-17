6e1c6d2bc07b3301552b7b7f7a93e0fc22505f48
SvenLukassen <71136134+SvenLukassen@users.noreply.github.com>
2022-11-17 12:35:20 +0100

    Hallo ik ben jara

--- src/rsid_pmid.py
+++ src/rsid_pmid.py
@@ -5,7 +5,7 @@ import pandas as pd
 import xml.etree.ElementTree as elt
 from collections import defaultdict
 
-content = elt.parse("C:/Users/jaral/OneDrive/Bureaublad/clinvar_cut.xml")
+content = elt.parse("C:/Users/swans/Downloads/clinvar_cut.xml")
 tree = content.getroot()
 no_rs = ""
 rsandpmids = {}
@@ -15,25 +15,24 @@ myDict = dict()
 
 
 def get_id_info():
-    global res
-    d = defaultdict(list)
+    dictionary = {}
     for i in rsid:
         no_rs = i.replace("rs", "")
         for s in tree.iter("ClinVarSet"):
             for movie in s.iter("XRef"):
                 for i in movie.findall('[@Type = "rs"]'):
+                    pmids = []
                     for a in i.findall(f'[@ID = "{no_rs}"]'):
                         if a.attrib["ID"] == no_rs:
-                            rsids.append(no_rs)
+                            print(no_rs)
+                            dictionary.update({no_rs: ""})
                     for t in s.iter("ID"):
                         for l in t.findall("[@Source='PubMed']"):
-                            if l.text not in pmids: pmids.append(l.text)
-                            res = {}
-                        for key in rsids:
-                            for value in pmids:
-                                res[key] = value
-                                break
-    return str(res)
+                            if l.text not in pmids:
+                                pmids.append(l.text)
+                                print(pmids)
+    print(dictionary)
+    # return str(res)
 
 
 rsid = ['rs267606900', 'rs267607078', 'rs12075']
