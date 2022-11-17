41e4d8898eb22309664f40795bf9a15b1898323f
jara <jam.laureijssen@student.han.nl>
2022-11-17 11:57:44 +0100

    pmid's

--- src/rsid_pmid.py
+++ src/rsid_pmid.py
@@ -1,29 +1,47 @@
+#!/usr/bin/env python
+
+
 import pandas as pd
 import xml.etree.ElementTree as elt
+from collections import defaultdict
 
-content = elt.parse("C:/Users/jaral/OneDrive/Bureaublad/clinvar_cut.xml").getroot()
+content = elt.parse("C:/Users/jaral/OneDrive/Bureaublad/clinvar_cut.xml")
+tree = content.getroot()
+no_rs = ""
+rsandpmids = {}
+rsids = []
+pmids = []
+myDict = dict()
 
 
-def get_id_info(inputID):
-    for child in content:
-        if child.find('id').text == inputID:
-            print(child.find('ID Source = "PubMed"').text)
+def get_id_info():
+    global res
+    d = defaultdict(list)
+    for i in rsid:
+        no_rs = i.replace("rs", "")
+        for s in tree.iter("ClinVarSet"):
+            for movie in s.iter("XRef"):
+                for i in movie.findall('[@Type = "rs"]'):
+                    for a in i.findall(f'[@ID = "{no_rs}"]'):
+                        if a.attrib["ID"] == no_rs:
+                            rsids.append(no_rs)
+                    for t in s.iter("ID"):
+                        for l in t.findall("[@Source='PubMed']"):
+                            if l.text not in pmids: pmids.append(l.text)
+                            res = {}
+                        for key in rsids:
+                            for value in pmids:
+                                res[key] = value
+                                break
+    return str(res)
 
 
 rsid = ['rs267606900', 'rs267607078', 'rs12075']
-
 
 # Syntax xml clinvar:
 # <XRef Type="rs" ID="267606900" DB="dbSNP"/>
 # <ID Source="PubMed">20970105</ID>
 
-def read_rsid(rsid):
-    for i in rsid:
-        no_rs = i.replace("rs", "")
-        print(no_rs)
-
 
 if __name__ == '__main__':
-    read_rsid(rsid)
-    get_id_info('267606900')
-
+    print(get_id_info())
