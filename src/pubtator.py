7d078776eb06d805ecbe9cc22b1e182b4dc30c74
jara <jam.laureijssen@student.han.nl>
2022-11-22 13:45:12 +0100

    snowball stemmer werkt.

--- src/pubtator.py
+++ src/pubtator.py
@@ -1,4 +1,3 @@
-
 import urllib.request
 import nltk
 from nltk.stem import PorterStemmer
@@ -38,7 +37,8 @@ def parse_pubtator(text):
     for line in lines:
         if len(line.split("\t")) == 6:
             term_line = line.split("\t")
-            terms.append(term_line[3].lower())
+            x = sn_stemmer.stem(term_line[3])
+            terms.append(x.lower())
             bioconcepts.append(term_line[4].lower())
             if term_line[3] not in terms_bioconcepts.keys():
                 terms_bioconcepts[
@@ -47,18 +47,9 @@ def parse_pubtator(text):
     terms_occurances = count_terms(terms)
     return terms, bioconcepts, terms_occurances
 
-words = ["cancers", "cancerous"]
+
 sn_stemmer = SnowballStemmer("english")
 
-#clearfileout
-with open("output_stem.txt",'w')as file:
-    pass
-
-for w in words:
-    stem = print(w, " : ", sn_stemmer.stem(w))
-    print(sn_stemmer.stem(w), file = open("output_stem.txt", "a"))
-    f = open("output_stem.txt", "r")
-    f.close()
 
 def count_terms(terms):
     terms_occurances = {}
@@ -69,24 +60,6 @@ def count_terms(terms):
     print(terms_occurances)
     return terms_occurances
 
-#count stem words
-file_name = "output_stem.txt"
-file_handle = open("output_stem.txt", "r")
-countWord = 0
-for content in file_handle:
-    chk = 0
-    content_length = len(content)
-    for i in range (content_length):
-        if content [i] ==' ':
-            if chk!=0:
-                countWord = countWord+1
-            chk = 0
-        else:
-            chk = chk+1
-    if chk!=0:
-        countWord = countWord+1
-print("\nstem word(s): " + sn_stemmer.stem(w) + ", occurance: ")
-print(countWord)
 
 if __name__ == '__main__':
     all_pmids = [20964851, 19176549, 28244479, 30713326, 31761807, 26891021]
