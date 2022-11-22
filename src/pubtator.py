a1b6a60c00921378a0e5b40ef680d0429599689e
jara <jam.laureijssen@student.han.nl>
2022-11-22 14:18:51 +0100

    Docstrings

--- src/pubtator.py
+++ src/pubtator.py
@@ -4,27 +4,44 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import SnowballStemmer
 
+sn_stemmer = SnowballStemmer("english")
 
-def get_terms(all_pmids):
-    input_type = "pmids"
-    export_type = "pubtator"
+
+def get_terms(pmids):
+    """
+    This funtion sents all the pmids to the pubtator API and gives the result to function 'analyze_pubtator'
+    :param pmids: All the pmids acquired from the input files
+    """
+    # Declaring variables to sent to pubtator api
+    input = "pmids"
+    export = "pubtator"
     bioconcepts = "disease,gene,mutation"
 
-    for pmid in all_pmids:
+    # Sending request to pubtator
+    for pmid in pmids:
         url = f"https://www.ncbi.nlm.nih.gov/research/" \
-              f"pubtator-api/publications/export/{export_type}?" \
-              f"{input_type}={pmid}&concepts={bioconcepts}"
+              f"pubtator-api/publications/export/{export}?" \
+              f"{input}={pmid}&concepts={bioconcepts}"
 
-        text = urllib.request.urlopen(url).read().decode("utf-8")
+        # Get pubtator results
+        pubtator_result = urllib.request.urlopen(url).read().decode("utf-8")
+        analyze_pubtator(pubtator_result)
 
-        parse_pubtator(text)
 
-
-def parse_pubtator(text):
+def analyze_pubtator(pubtator_result):
+    """
+    This function analyses the results from pubtator.
+    It will filter out the terms found in pubtator, the abstract and title
+    The terms will be sent to function terms_occurances to count the terms
+    :param pubtator_result: Result from pubtator api
+    :return terms[] = all the terms found in pubtator result
+            bioconcepts[] = all the bioconcepts found in pubtator api
+            terms_occurances{} = all the terms and their count
+    """
     terms = []
     bioconcepts = []
     terms_bioconcepts = {}
-    lines = text.split("\n")
+    lines = pubtator_result.split("\n")
 
     try:
         title = lines[0].split('|')[2].rstrip()
@@ -48,10 +65,12 @@ def parse_pubtator(text):
     return terms, bioconcepts, terms_occurances
 
 
-sn_stemmer = SnowballStemmer("english")
-
-
 def count_terms(terms):
+    """
+    This function counts the terms and their occurances
+    :param terms: all the terms from pubtator
+    :return: terms_occurances{} = terms and their occurances
+    """
     terms_occurances = {}
     for term in terms:
         if term not in terms_occurances.keys():
@@ -63,6 +82,4 @@ def count_terms(terms):
 
 if __name__ == '__main__':
     all_pmids = [20964851, 19176549, 28244479, 30713326, 31761807, 26891021]
-    get_terms(all_pmids)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    get_terms(pmids=all_pmids)
