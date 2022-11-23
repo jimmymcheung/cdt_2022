20ae6ac62a9bf568eac20c9cb3cd0ab9cd16b938
jimmymcheung <64972743+jimmymcheung@users.noreply.github.com>
2022-11-23 12:58:38 +0100

    add check for sub-module

--- src/module_check.py
+++ src/module_check.py
@@ -2,9 +2,21 @@
 
 # Works on Python3 >= 3.6
 # check if required python modules are installed
+import json
 import sys
 
-mod_list = ["re", "alive_process", "requests"]
+mod_list = ["requests", "pandas"]
+submod_list = ["""{
+    "parent": "alive_progress",
+    "child": "alive_bar"
+}"""]
+
+
+# class NestMod:
+#     def __int__(self, parent, child):
+#         self.parent = parent
+#         self.child = child
+
 
 for i in mod_list:
     try:
@@ -17,6 +29,20 @@ for i in mod_list:
         sys.exit(1)
 
 
+# for x in submod_list:
+#     submod = json.load(json.dump(x))
+#     parent = submod["parent"]
+#     child = submod["child"]
+#     try:
+#         __import__(child, fromlist=parent)
+#         print("\033[0mINFO: \'" + parent + "." + child + "\' is found.\033[0m")
+#     except ModuleNotFoundError as err:
+#         # Error handling
+#         # print(err)
+#         print("\033[1mERROR: \'" + parent + "." + child + "\' is not found\033[0m")
+#         sys.exit(1)
+
+
 # Python3 >= 3.4 compatibility
 # import importlib
 # importlib.util.find_spec(name, package=None)
@@ -27,5 +53,5 @@ for i in mod_list:
 
 
 # for n in submod_list:
-#    pkg_spec = importlib.util.find_spec(pakcage="alive.bar")
+#    pkg_spec = importlib.util.find_spec(package="alive.bar")
 #    found = pkg_spec is not None
