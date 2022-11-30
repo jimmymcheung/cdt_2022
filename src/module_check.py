3ae0f0de933df1d49ab0f67c33c37eef882bc6c0
jimmymcheung <64972743+jimmymcheung@users.noreply.github.com>
2022-11-30 11:18:46 +0100

    merge depolyment file from 'rsid_pmid' to 'main'

---
+++ src/module_check.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+# Check existence of required python modules
+# Copyright © 2022 CDT project
+# Author: Jiaming Zhang
+# Works on Python3 >= 3.6
+import sys
+import xml.etree.ElementTree
+
+mod_list = ["requests", "pandas", "nltk"]
+subMod_list = """<?xml version="1.0"?>
+<modlist>
+    <module>
+        <pipName>alive-progress</pipName>
+        <parent>alive_progress</parent>
+        <child>alive_bar</child>
+    </module>
+    <module>
+        <pipName>lxml</pipName>
+        <parent>lxml</parent>
+        <child>etree</child>
+    </module>
+    <module>
+        <pipName>nltk</pipName>
+        <parent>nltk.stem</parent>
+        <child>PorterStemmer</child>
+    </module>
+    <module>
+        <pipName>nltk</pipName>
+        <parent>nltk.tokenize</parent>
+        <child>word_tokenize</child>
+    </module>
+    <module>
+        <pipName>nltk</pipName>
+        <parent>nltk.stem.snowball</parent>
+        <child>SnowballStemmer</child>
+    </module>
+</modlist>"""
+err_count = 0
+
+
+for i in mod_list:
+    try:
+        exec("import " + i)
+        print("\033[0mINFO: \'" + i + "\' is found.\033[0m")
+    # except ModuleNotFoundError as err:
+    except ImportError:
+        # Error handling
+        print("\033[1mERROR: \'" + i + "\' is not found.\033[0m")
+        err_count = err_count + 1
+
+
+subMod_xml = xml.etree.ElementTree.fromstring(subMod_list)
+for e in subMod_xml:
+    parent = e.find('parent').text
+    child_list = e.findall('child')
+    for c in child_list:
+        child = c.text
+        try:
+            exec("from " + parent + " import " + child)
+            # __import__(parent, fromlist=child)
+            print("\033[0mINFO: \'" + parent + '.' + child + "\' is found.\033[0m")
+        except ImportError:
+            # Error handling
+            print("\033[1mERROR: \'" + parent + '.' + child + "\' is not found\033[0m")
+            err_count = err_count + 1
+
+
+if err_count > 0:
+    if err_count == 1:
+        print("\033[4mINFO: " + str(err_count) + " module is missing.")
+    elif err_count > 1:
+        print("\033[4mINFO: " + str(err_count) + " (sub)modules are missing.")
+    sys.exit(1)
