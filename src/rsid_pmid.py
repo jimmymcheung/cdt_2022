b3739664fbdce2971831b8bf9b5893fa4f419191
SvenLukassen <71136134+SvenLukassen@users.noreply.github.com>
2022-12-01 12:01:07 +0100

    3loops

--- src/rsid_pmid.py
+++ src/rsid_pmid.py
@@ -2,26 +2,10 @@
 
 # RSID local query for PMID
 # Copyright © 2022 CDT project
-# Author: Jara Laureijssen
+# Author: Jara Laureijssen, Sven Lukassen
 # See also: 'search_rsid.py' for querying on ClinVar Server.
 
-# import os
-# import pandas as pd
-import xml.etree.ElementTree as elt
-# from collections import defaultdict
-# from alive_progress import alive_bar
-# from tqdm import tqdm
-# from time import sleep
 from lxml import etree as et
-
-content = elt.parse("../res/clinvar/clinvar_cut.xml")
-tree = content.getroot()
-fileInput = input('Please specify your GT file: \n')
-no_rs = ""
-rsandpmids = {}
-rsids = []
-pmids = []
-myDict = dict()
 
 
 def read_gt(filename):
@@ -67,32 +51,31 @@ def data_to_database(dictionary):
 # <XRef Type="rs" ID="267606900" DB="dbSNP"/>
 # <ID Source="PubMed">20970105</ID>
 
-def fn2(rsid):
+def fn2(rsid, fileInput):
+    dictionary = {}
     pmids = []
     no_rs_no_duplicate = ''
-    dictionary2 = {}
-    context = et.iterparse("../res/clinvar/clinvar_cut.xml", tag=['XRef', 'ID'])
+    context = et.iterparse(fileInput, tag=['XRef', 'ID'])
     for event, element in context:
-        print(element.attrib)
-        for i in rsid:
-            no_rs = i.replace("rs", "")
-            for z in element.findall('[@Type = "rs"]'):
+        for z in element.findall('[@Type = "rs"]'):
+            for i in rsid:
                 no_rs = i.replace("rs", "")
                 pmids = []
-                for a in z.findall(f'[@ID = "{no_rs}"]'):
-                    if a.attrib["ID"] == no_rs:
-                        dictionary2.update({no_rs: ""})
-                        no_rs_no_duplicate = no_rs
-            for l in element.findall("[@Source='PubMed']"):
-                if l.text not in pmids:
-                    if no_rs_no_duplicate != "":
-                        pmids.append(l.text)
-                        dictionary2.update({no_rs_no_duplicate: pmids})
-    return dictionary2
+                if z.attrib["ID"] == no_rs:
+                    dictionary.update({no_rs: ""})
+                    no_rs_no_duplicate = no_rs
+        for l in element.findall("[@Source='PubMed']"):
+            if l.text not in pmids:
+                if no_rs_no_duplicate != "":
+                    pmids.append(l.text)
+                    dictionary.update({no_rs_no_duplicate: pmids})
+
+    return dictionary
 
 
 if __name__ == '__main__':
-    # rsidlist, iidlist = read_gt(
-    #     "../res/GT_files/Original_files/uk4CA868_20180206095657(1).gt")
+    fileInput = input("PathName for GT File: \n")
+    clinvarfile = ("../../clinvar/ClinVarFullRelease_2022-10.xml")
     rsidlist, iidlist = read_gt(fileInput)
-    print(fn2(rsidlist))
+    dictionary = fn2(rsidlist, clinvarfile)
+
